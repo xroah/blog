2aedf8298c454141190989a3d14be41efbd78d28
xroah <396069583@qq.com>
2018-08-12 22:07:08 +0800

    checkbox component

---
+++ src/components/common/checkbox/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <label class="checkbox-wrapper">
+        <input
+         type="checkbox" 
+        :checked="checked"
+        v-model="check"
+        value="1"
+         class="input-checkbox"
+         @change="change"/>
+        <span class="checkbox-outer"></span>
+        <span>{{text}}</span>
+    </label>
+</template>
+<style lang="scss" src="./index.scss"></style>
+<script>
+export default {
+    data() {
+        return {
+            check: this.checked
+        }
+    },
+    props: ["text", "checked"],
+    methods: {
+        change() {
+            this.$emit("onChange", this.check);
+        }
+    }
+};
+</script>
+
