abf1d04c9cb4ba6d2bf76b2a73e3e282efce1759
xroah <396069583@qq.com>
2018-08-15 12:57:36 +0800

    add radio component

---
+++ src/components/common/radio/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <label class="radio-wrapper">
+        <input 
+            type="radio"
+            v-model="check"
+            :value="value"
+            class="input-radio"
+            :name="name"
+            @change="change($event)">
+        <span class="radio-border">
+
+        </span><slot></slot>
+    </label>
+</template>
+
+<style src="./index.scss"></style>
+
+<script>
+export default {
+    props: ["name", "onChange", "checked", "value"],
+    data() {
+        return {
+            check: this.checked
+        }
+    },
+    methods: {
+        change() {
+            this.$emit("onChange", this.check);
+        }
+    }
+}
+</script>
+
+
