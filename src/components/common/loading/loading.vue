18ca70d09749bc277eec1e713c9193c77414be75
xroah <396069583@qq.com>
2018-08-19 09:48:35 +0800

    add inline mode

--- src/components/common/loading/loading.vue
+++ src/components/common/loading/loading.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="loading-wrapper">
-        <div class="center-box">
-           <div class="rotate">
+    <div class="loading-wrapper" :class="fullscreen ? 'loading-fullscreen' : ''">
+        <div class="center-box" v-if="fullscreen">
+            <div class="rotate">
                 <div class="item"></div>
                 <div class="item"></div>
                 <div class="item"></div>
@@ -10,9 +10,25 @@
                 <div class="item"></div>
                 <div class="item"></div>
                 <div class="item"></div>
-            </div> 
+            </div>
+        </div>
+        <div v-else>
+            <span class="loading-spinner"></span>
+            <slot></slot>
         </div>
     </div>
 </template>
+
 <style lang="scss" src="./index.scss"></style>
+
+<script>
+export default {
+   props: {
+       fullscreen: {
+           default: true
+       }
+   } 
+}
+</script>
+
 
