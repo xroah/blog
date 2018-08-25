cad83c807bfbc0495e6d5b7cac9b304be6799ccc
xroah <396069583@qq.com>
2018-08-25 18:08:23 +0800

    404 error page for vue-router

---
+++ src/components/common/error/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="error-container">
+        <img src="../../../assets/images/404.png">
+        <p>抱歉,您访问的页面不存在</p>
+        <div>
+            <v-button type="primary" @click="toHome">返回首页</v-button>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .error-container {
+        overflow: hidden;
+        height: 100vh;
+        background-color: #fff;
+        color: #999;
+        text-align: center;
+        img {
+            width: 280px;
+        }
+        .v-btn {
+            margin-top: 30px;
+        }
+    }
+</style>
+
+<script>
+import VButton from "../button";
+export default {
+    components: {
+        VButton
+    },
+    methods: {
+        toHome() {
+            this.$router.push("/");
+        }
+    }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,24 @@
-const PublicMain = () => import("../components/public/main");
-const PublicHome = () => import("../components/public/home");
-const About = () => import("../components/public/about");
+const PublicMain = () =>
+    import ("../components/public/main");
+const PublicHome = () =>
+    import ("../components/public/home");
+const About = () =>
+    import ("../components/public/about");
 
-const AdminLogin = () => import("../components/admin/login");
-const AdminMain = () => import("../components/admin/main");
-const AdminHome = () => import("../components/admin/home");
-const AddArticle = () => import("../components/admin/addEditArticle");
-const ArticleClassify = () => import("../components/admin/classify");
-const ArticleDetails = () => import("../components/admin/articleDetails");
+const AdminLogin = () =>
+    import ("../components/admin/login");
+const AdminMain = () =>
+    import ("../components/admin/main");
+const AdminHome = () =>
+    import ("../components/admin/home");
+const AddArticle = () =>
+    import ("../components/admin/addEditArticle");
+const ArticleClassify = () =>
+    import ("../components/admin/classify");
+const ArticleDetails = () =>
+    import ("../components/admin/articleDetails");
+const Error404 = () =>
+    import ("../components/common/error");
 
 export default [{
     path: "/",
@@ -29,18 +40,21 @@ export default [{
         path: "article",
         component: AdminHome,
         name: "adminHome"
-    },{
+    }, {
         path: "article/add",
         name: "addArticle",
         component: AddArticle
-    },{
+    }, {
         path: "article/edit/:id?",
         component: AddArticle
-    },{
+    }, {
         path: "article/details/:id?",
         component: ArticleDetails
-    },{
+    }, {
         path: "classify",
         component: ArticleClassify
     }]
+}, {
+    path: "*",
+    component: Error404
 }];
