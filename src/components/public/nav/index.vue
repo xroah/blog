5a697981f31dc59ea9600487491cd319caf1e752
xroah <396069583@qq.com>
2018-09-12 17:00:45 +0800

    use router-link exact-active-class

--- src/components/public/nav/index.vue
+++ src/components/public/nav/index.vue
@@ -1,13 +1,13 @@
 <template>
     <v-nav>
         <li>
-            <router-link class="nav-link" exact active-class="active" to="/">首页</router-link>
+            <router-link class="nav-link" exact-active-class="active" to="/">首页</router-link>
         </li>
         <li>
-            <router-link class="nav-link" active-class="active" to="/article">文章</router-link>
+            <router-link class="nav-link" exact-active-class="active" to="/article">文章</router-link>
         </li>
         <li>
-            <router-link class="nav-link" exact active-class="active" to="/about">关于我</router-link>
+            <router-link class="nav-link" exact-active-class="active" to="/about">关于我</router-link>
         </li>
         <nav-right slot="right"></nav-right>
     </v-nav>
