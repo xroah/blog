09e28c48897a86e1f1fcacb85aa2fb59c62c77ec
xroah <396069583@qq.com>
2018-11-12 00:01:12 +0800

    material-ui

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,10 +1,20 @@
-import * as React from "react";
+import * as React from "react"; 
+import Button from '@material-ui/core/Button';
+import "./index.scss";
 
 export default class Nav extends React.Component {
+    constructor(props: any) {
+        super(props);
+    }
+
     render() {
+        console.log(this.props)
         return (
             <div>
                 nav
+                <Button color="primary" variant="contained" classes={{
+                    containedPrimary: "primary"
+                }}>按钮</Button>
             </div>
         );
     }
