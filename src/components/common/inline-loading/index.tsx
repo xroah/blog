8d7bcc4570f7451c35718ad680259ea54c74d4f0
xroah <396069583@qq.com>
2020-03-24 13:02:47 +0800

    remove all for code refactoring

--- src/components/common/inline-loading/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { CircularProgress } from "@material-ui/core";
-
-export default () => (
-    <div className="flex-center" style={{ padding: 10 }}>
-        <CircularProgress size={20} />
-        <span style={{ marginLeft: 10 }}>正在加载...</span>
-    </div>
-);
--- src/components/common/user-confirmation.ts
+++
@@ -1,9 +0,0 @@
-import hint from "./hint-dialog";
-
-
-export default function getUserConfirmation(msg: string, callback: (arg: boolean) => void) {
-    hint.confirm(msg, {
-        onOk: () => callback(true),
-        onCancel: () => callback(false)
-    });
-}
