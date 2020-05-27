605cffea4d7146a24e2c3fc28e465fbbdd9bd0a0
xroah <396069583@qq.com>
2020-05-27 22:15:25 +0800

    article list

--- admin/js/index.js
+++ admin/js/index.js
@@ -1,1 +1,12 @@
 import "./modules/nav.js";
+import request from "./modules/request.js";
+import { ARTICLE } from "./modules/api.js";
+
+function fetchArticleList(param) {
+    request(ARTICLE)
+    .then(res => {
+        console.log(res);
+    });
+}
+
+fetchArticleList();
