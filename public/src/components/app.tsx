665a412a49c14ee51ff5dc62d6585e504fd9190c
xroah <396069583@qq.com>
2020-06-09 12:41:53 +0800

    use cdn and revise import

--- public/src/components/app.tsx
+++ public/src/components/app.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Container, Alert } from "reap-ui";
+import Container from "reap-ui/lib/Layout/Container";
+import Alert from "reap-ui/lib/Alert";
 import { hot } from "react-hot-loader/root";
 import Header from "./header";
 
--- public/src/index.tsx
+++ public/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, hydrate } from "react-dom";
 import App from "./components/app";
-import "bootstrap/dist/css/bootstrap.css";
+import "./index.scss";
 
 const root = document.getElementById("root");
 
