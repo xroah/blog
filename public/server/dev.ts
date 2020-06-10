07c5c7810ae7d9407c637315690e54f1e09d6909
xroah <396069583@qq.com>
2020-06-10 22:34:14 +0800

    history fallback

--- public/server/dev.ts
+++ public/server/dev.ts
@@ -2,6 +2,7 @@ import webpack from "webpack";
 import devMiddleware from "webpack-dev-middleware";
 import hotMiddleware from "webpack-hot-middleware";
 import express from "express";
+import history from "connect-history-api-fallback";
 import conf from "../config/dev.conf";
 import createProxy from "./proxy";
 
@@ -11,6 +12,7 @@ const compiler = webpack(conf);
 process.env.NODE_ENV = "development";
 
 createProxy(app);
+app.use(history());
 app.use(devMiddleware(compiler));
 app.use(hotMiddleware(compiler));
 
