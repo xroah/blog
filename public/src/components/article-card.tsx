bd926ea68b6eaac384eefba283577dcf4d8caa0a
xroah <396069583@qq.com>
2020-06-11 22:21:24 +0800

    update

---
+++ public/src/components/article-card.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Card from "reap-ui/lib/Card";
+import Button from "reap-ui/lib/Button";
+import EyeIcon from "./icons/eye";
+import MessageSquare from "./icons/message-square";
+import { createUseStyles } from "react-jss";
+
+const useStyle = createUseStyles({
+    "article-card": {
+        transition: "all .3s",
+        marginTop: 15,
+
+        "&:hover": {
+            transform: "translateY(-5px)",
+            boxShadow: "1px 1px 5px 0 rgba(0, 0, 0, .3)"
+        }
+    },
+    "article-title": {
+        
+        "& .card-title": {
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            fontSize: 14,
+            whiteSpace: "nowrap"
+        },
+
+        "& .card-subtitle": {
+            fontSize: 12
+        }
+    }
+});
+
+interface ArticleCardProps {
+    children: React.ReactNode;
+    viewed: number;
+    comments: number;
+    articleId: string;
+    title: string;
+    time: string;
+}
+
+export default function ArticleCard(props: ArticleCardProps) {
+    const classes = useStyle();
+    const {
+        viewed,
+        comments,
+        articleId,
+        title,
+        time,
+        children
+    } = props;
+    const viewArticle = () => window.open(`/view/${articleId}`)
+
+    return (
+        <Card
+            body
+            className={classes["article-card"]}
+            footer={
+                <div className="d-flex align-items-center">
+                    <EyeIcon />
+                    <span className="ml-1">{viewed}</span>
+                    <MessageSquare className="ml-3" />
+                    <span className="ml-1">{comments}</span>
+                    <Button
+                        style={{ marginLeft: "auto" }}
+                        onClick={viewArticle}>查看全文>></Button>
+                </div>
+            }>
+            <Card.Title
+                className={classes["article-title"]}
+                subtitle={time}>{title}</Card.Title>
+            <Card.Text>{children}...</Card.Text>
+        </Card>
+    )
+}
--- public/src/components/home.tsx
+++ public/src/components/home.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import ArticleCard from "./article-card";
 
-export default () => {
-    return <div>首页</div>;
+export default class HomePage extends React.Component {
+    render() {
+        return (
+            <div>
+                <ArticleCard
+                articleId="dsfsdf32423234dsf"
+                title="外文外文三段论法角度看斯拉夫"
+                time="2020-06-11 18:23"
+                comments={999}
+                viewed={99999}>
+                    苏联的开发角色的快乐佛教阿隆索的法律今拉萨的弗拉斯地方将斯拉夫阿里的司法局拉斯的佛教萨的了佛教的斯拉夫将了第三咖啡碱阿的苏联军方苏联看到飞机萨拉丁教父的思考辣椒粉啊第三了佛教阿斯顿了开发将阿萨拉丁教父
+                </ArticleCard>
+            </div>
+        )
+    }
 }
