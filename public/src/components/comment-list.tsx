e1e1accec0472899a9454516d94c236ffb232921
xroah <396069583@qq.com>
2020-06-16 22:46:15 +0800

    comment list

--- public/src/components/comment-list.tsx
+++ public/src/components/comment-list.tsx
@@ -3,6 +3,7 @@ import { createUseStyles } from "react-jss";
 import Comment from "../components/publish-comment";
 import Spinner from "reap-ui/lib/Spinner";
 import { Collapse } from "reap-ui/lib/Collapse";
+import { format } from "fecha";
 
 interface Props {
     list: any[];
@@ -15,7 +16,7 @@ interface Props {
 const useStyles = createUseStyles({
     "comment-wrapper": {
         borderTop: "1px solid #eee",
-        marginTop: 20
+        margin: "20px 0"
     },
     "sub-comment-wrapper": {
         paddingLeft: 20
@@ -25,32 +26,105 @@ const useStyles = createUseStyles({
 interface ItemProps {
     comment: any;
     articleId: string;
+    isChild?: boolean;
+    replyTo: any;
 }
 
 const useItemStyles = createUseStyles({
     "comment-item": {
+        marginBottom: 15,
+        borderBottom: "1px solid #eee",
 
+        "& .publish-time": {
+            fontSize: 12
+        },
+
+        "& .home-link": {
+            display: "inline-block",
+            maxWidth: 100,
+            textDecoration: "none"
+        }
     }
 });
 
 function CommentItem(props: ItemProps) {
     const {
         comment,
-        articleId
+        articleId,
+        isChild = false,
+        replyTo
     } = props;
     const classes = useItemStyles();
     const [visible, updateVisible] = useState(false);
     const handleCancel = () => updateVisible(false);
+    const handleUrl = (url?: string) => {
+        if (!url) return;
+
+        if (url.startsWith("http") || url.startsWith("//")) {
+            return url;
+        }
+
+        return `//${url}`;
+    }
+    const renderUser = () => {
+        let el = null;
+        const DEFAULT_NAME = "匿名用户";
+
+        if (isChild) {
+            el = (
+                <>
+                    <a
+                        href={handleUrl(comment.homePage)}
+                        target="blank"
+                        className="mr-1 home-link text-truncate">
+                        {comment.userName || DEFAULT_NAME}
+                    </a>
+                    <span className="mr-1 text-secondary">回复</span>
+                    <a
+                        href={handleUrl(replyTo.homePage)}
+                        className="home-link text-truncate"
+                        target="blank">
+                        {replyTo.userName || DEFAULT_NAME}
+                    </a>
+                </>
+            );
+        } else {
+            el = (
+                <a
+                    href={handleUrl(comment.homePage)}
+                    className="home-link text-truncate"
+                    target="blank">
+                    {comment.userName || DEFAULT_NAME}
+                </a>
+            );
+        }
+
+        return <div>{el}</div>;
+    };
+    let dateStr = "";
+
+    try {
+        dateStr = format(new Date(comment.createTime), "YYYY-MM-DD HH:mm");
+    } catch (error) {
+        console.log(comment)
+    }
 
     return (
         <>
             <div className={classes["comment-item"]}>
-                {comment.content}
+                <div>
+                    {renderUser()}
+                    <div className="text-muted publish-time">
+                        {dateStr}
+                    </div>
+                </div>
+                <p className="mt-2">{comment.content}</p>
                 <Collapse isOpen={visible}>
                     <Comment
                         articleId={articleId}
                         replyTo={comment._id}
                         root={comment.root || comment._id}
+                        showCancel
                         onCancel={handleCancel} />
                 </Collapse>
             </div>
@@ -68,29 +142,43 @@ class CommentList extends React.Component<Props & { classes: any }> {
         fetchComments(articleId);
     }
 
-    renderList(list: any) {
+    renderList(list: any, isChild = false, parent: any = null) {
         const {
             articleId,
             classes
         } = this.props;
+        const map = new Map();
+
+        if (parent) {
+            map.set(parent._id, parent);
+        }
+
+        list.forEach((l: any) => {
+            map.set(l._id, l);
+        });
 
         return list.map((l: any) => {
             const c = l.children || [];
 
+            if (isChild && !map.has(l.replyTo)) {
+                return null;
+            }
+
             return (
-                <>
+                <div key={l._id}>
                     <CommentItem
-                        key={l._id}
                         comment={l}
-                        articleId={articleId} />
+                        replyTo={map.get(l.replyTo)}
+                        articleId={articleId}
+                        isChild={isChild} />
                     {
                         !!c.length && (
                             <div className={classes["sub-comment-wrapper"]}>
-                                {this.renderList(c as any)}
+                                {this.renderList(c as any, true, l)}
                             </div>
                         )
                     }
-                </>
+                </div>
             );
         });
     }
@@ -104,7 +192,7 @@ class CommentList extends React.Component<Props & { classes: any }> {
 
         return (
             <div className={classes["comment-wrapper"]}>
-                <h4 className="my-3">全部评论：{list.length}</h4>
+                <h4 className="my-3">全部评论</h4>
                 {this.renderList(list)}
                 {
                     loading && (
